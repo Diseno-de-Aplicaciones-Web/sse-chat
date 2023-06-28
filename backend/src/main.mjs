1af7be78c76a27f7e5ee452fa27666fb4a7bc808
Daniel Bañobre Dopico <daniel.banobre.dopico@pm.me>
2023-06-28 18:30:39 +0200

    ...

--- backend/src/main.mjs
+++ backend/src/main.mjs
@@ -4,25 +4,35 @@ import cors from "cors"
 const app = express()
 app.use(cors())
 
-function intermediarioDeAutorizacion(peticion, resposta, seguinteIntermediario){
-    JSON.parse(peticion.headers.authorization.split(" ")[1])
-}
+const clientesSSE = new Map()
 
-app.get('/chat', async function(req, res) {
+app.get('/chat', (peticion, resposta)=>{
     /**
+     * Necesitamos enviar respuestas individualizadas a los diferentes usuarios que
+     * se conectan, así que hemos de mantener un directorio de los diferentes usuarios
+     * el objeto de respuesta correspondiente a cada uno de ellos.
+     */ 
+    const idUsuario = peticion.body.id
+    clientesSSE.set(idUsuario, resposta)
+    /** 
+     * Enviamos los encabezados de la respuesta al cliente
      * Los encabezados Content-Type y Connection son los que consiguen que
      * este endpoint pueda servir como una fuente de SSE.
      * 'Connection': 'keep-alive' indica al navegador que no se desconecte tras la peticion.
      * 'Content-Type': 'text/event-stream' informa al navegador que se trata de una fuente SSE.
      * La caché es desactivada porque no tiene sentido en este tipo de conexión.
      */
-    res.set({
-        'Cache-Control': 'no-cache',
-        'Content-Type': 'text/event-stream',
-        'Connection': 'keep-alive'
-    });
-    res.flushHeaders(); //Fuerza el envío inmediato al navegador de los encabezados anteriores.
+    resposta.writeHead(
+        200,
+        {
+            'Cache-Control': 'no-cache',
+            'Content-Type': 'text/event-stream',
+            'Connection': 'keep-alive'
+        }
+    )
 });
+
+app.get("/chat", )
 
 app.listen( process.env.PORT ?? 8000, ()=>{
     console.log("Funcionando...");
