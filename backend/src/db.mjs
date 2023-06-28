118e0eef83f145901d900b4500cca020c5116d4d
Daniel Bañobre Dopico <daniel.banobre.dopico@pm.me>
2023-06-28 23:31:18 +0200

    Provides DB models

---
+++ backend/src/db.mjs
@@ -0,0 +1,28 @@
+import { DataTypes, Sequelize } from "sequelize"
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite'
+});
+
+const Usuario = sequelize.define("Usuario", {
+    nome: DataTypes.STRING,
+    contrasinal: DataTypes.STRING
+})
+
+const Mensaxes = sequelize.define("Mensaxes",{
+    contido:  DataTypes.STRING,
+    lido: DataTypes.BOOLEAN
+})
+
+Usuario.hasMany(Mensaxes, { as: "enviado", foreignKey: "remitente"})
+Usuario.hasMany(Mensaxes, { as: "recivido", foreignKey: "destinatario"})
+Mensaxes.belongsTo(Usuario, { foreignKey: "destinatario"})
+Mensaxes.belongsTo(Usuario, { foreignKey: "remitente"})
+
+sequelize.sync({alter: true})
+
+export {
+    Usuario,
+    Mensaxes
+}
